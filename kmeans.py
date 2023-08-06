2c0ab69407cd37f6e3d21b43748d27ffa916ff2f
mzawadzki-adamiak@virtuslab.com <zawmaciek@gmail.com>
2023-08-06 10:59:03 +0200

    kmeans(failed)

--- kmeans.py
+++ kmeans.py
@@ -1,20 +1,22 @@
 from collections import Counter
-from typing import List
-
-import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 from dataset import Dataset, movieId
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
-from validation import stereotypical_fans
+CLUSTERS_NUM = 150
 
 
 class KMeansSystem():
     def __init__(self) -> None:
+        self.matrix = None
+        self.scaler = None
         self.labels = None
         self.kmeans = None
+        self.pca = None
         self.dataset = Dataset()
         self.create_model()
 
@@ -23,27 +25,33 @@ class KMeansSystem():
         # SAMPLE RANDOM
         number_of_rows = matrix.shape[0]
         random_indices = np.random.choice(number_of_rows,
-                                          size=matrix.shape[1] // 5,
+                                          size=matrix.shape[1] // 100,
                                           replace=False)
         # display random rows
         self.matrix = matrix[random_indices, :]
-        scaler = StandardScaler()
+        self.scaler = StandardScaler()
         print("scaling")
-        scaled_features = scaler.fit_transform(self.matrix)
+        scaled_features = self.scaler.fit_transform(self.matrix)
+        print("pca")
+        self.pca = PCA(n_components=250)
+        pca_features = self.pca.fit_transform(scaled_features)
         print("training model")
-        self.kmeans = KMeans(n_clusters=150, random_state=0, n_init="auto")
-        self.labels = self.kmeans.fit(scaled_features)
+        self.kmeans = KMeans(n_clusters=CLUSTERS_NUM, random_state=0, n_init="auto")
+        self.labels = self.kmeans.fit(pca_features)
 
     def infer(self, ratings: list[tuple[movieId, float]]) -> list[str]:
         array = [0 for _ in range(self.dataset.movies_count)]
         for movie, rating in ratings:
             array[self.dataset.movie_id_to_matrix_movie_id[movie]] = rating
+        array = self.scaler.fit_transform([array])[0]
+        array = self.pca.transform([array])[0]
         label = self.kmeans.predict([array])[0]
         print(label)
         print(self.get_movies_from_label(label))
 
     def get_movies_from_label(self, label: int):
         indices = np.where(self.labels.labels_ == label)[0]
+        print(f"users : {len(indices)}")
         movies = self.matrix[indices]
         summed = np.sum(movies, axis=0)
         movies = []
@@ -52,20 +60,26 @@ class KMeansSystem():
         movies = sorted(movies, key=lambda x: x[1], reverse=True)[0:10]
         matrix_ids = [self.dataset.matrix_movie_id_to_movie_id[a[0]] for a in movies]
         all_movies = self.dataset.get_all_movies()
-        return [a for a in all_movies if a.movieId in matrix_ids]
+        return [a for a in all_movies if a.movieId in matrix_ids], len(indices)
 
 
 k = KMeansSystem()
-for fan in stereotypical_fans:
-    print(fan)
-    movies = [(movieId(a[0]), a[1]) for a in stereotypical_fans[fan]]
-    k.infer(movies)
-# for i in range(30):
-#     print(i)
-#     movies = k.get_movies_from_label(i)
-#     c = Counter()
-#     for movie in movies:
-#         c.update(movie.genres)
-#     print(c.most_common())
-#     for movie in movies:
-#         print(f"{movie.title}, {movie.movieId}")
+# for fan in stereotypical_fans:
+#     print(fan)
+#     movies = [(movieId(a[0]), a[1]) for a in stereotypical_fans[fan]]
+#     k.infer(movies)
+counts = []
+for i in range(CLUSTERS_NUM):
+    print(f"label {i}")
+    movies, count = k.get_movies_from_label(i)
+    counts.append(count)
+    c = Counter()
+    for movie in movies:
+        c.update(movie.genres)
+    print(c.most_common())
+    for movie in movies:
+        print(f"{movie.title}, {movie.movieId}")
+plt.plot(range(CLUSTERS_NUM), counts)
+plt.xlabel("nr klastra")
+plt.ylabel("ilość użytkowników")
+plt.show()
--- validation.py
+++ validation.py
@@ -1,5 +1,4 @@
 stereotypical_fans = dict()
-stereotypical_fans["comedy"] = [(1219, 1.0), (1085, 1.0), (491, 1.0),
-                                (1269, 1.0)]  # blues brothers, dial m for murder,manhattan murder mystery, back to the future
+stereotypical_fans["comedy"] = [(1219, 1.0), (1085, 1.0), (491, 1.0), (1269, 1.0)]  # blues brothers, dial m for murder,manhattan murder mystery, back to the future
 stereotypical_fans["children"] = [(33, 1.0), (593, 1.0), (594, 1.0), (1012, 1.0)]  # babe, snow white,beauty and the beast,parent trap
 stereotypical_fans["adventure"] = [(647, 1.0), (779, 1.0), (1100, 1.0), (1269, 1.0)]  # mission impossible, idenpenence day, top gun,back to the future
